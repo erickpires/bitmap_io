6b65693cba0024f798d048e70da8fbf8bd7d1f2f
Erick Pires <pires@erickpires.com>
2017-04-19 17:05:23 -0300

    Refactor image reading.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+#[macro_use]
+mod bitmap_read;
+
+use bitmap_read::BytesWalker;
+use bitmap_read::map_zero_based;
+
 use std::fmt::Display;
 use std::fmt::Formatter;
 
@@ -9,7 +15,6 @@ use std::cmp::max;
 use std::ops::Range;
 
 use std::convert;
-use std::intrinsics::transmute;
 
 const BMP_MAGIC_NUMBER : u16 = 0x4d_42; // "MB": We are little-endian
 
@@ -392,16 +397,6 @@ fn mask_offset_and_shifted(mut mask: u32) -> (u8, u32) {
     (offset, mask)
 }
 
-// TODO(erick): Floating-point is slow. We have enough
-// precision to do it using fixed-point math.
-fn map_zero_based(value: &mut u8, from: u32, to: u32) {
-    // Don't do useless work and don't divide by zero.
-    if from == to || from == 0 { return; }
-
-    let t = (*value as f32) / from as f32;
-    *value = (to as f32 * t) as u8;
-}
-
 fn interpret_image_data(data: &[u8],
                         info_header: &BitmapInfoHeader,
                         palette: &Option<BitmapPalette>) -> Vec<BitmapPixel> {
@@ -418,190 +413,40 @@ fn interpret_image_data(data: &[u8],
         let blue_mask  = info_header.blue_mask;
         let alpha_mask = info_header.alpha_mask;
 
-        let (red_offset,   red_shifted)   = mask_offset_and_shifted(red_mask);
-        let (green_offset, green_shifted) = mask_offset_and_shifted(green_mask);
-        let (blue_offset,  blue_shifted)  = mask_offset_and_shifted(blue_mask);
-        let (alpha_offset, alpha_shifted) = mask_offset_and_shifted(alpha_mask);
-
         if bits_per_pixel == 32 {
-            while data_walker.has_data() {
-                let pixel_value = data_walker.next_u32();
-
-                let mut pixel = BitmapPixel {
-                    blue  : ((pixel_value  >> blue_offset)  & 0xff) as u8,
-                    green : ((pixel_value  >> green_offset) & 0xff) as u8,
-                    red   : ((pixel_value  >> red_offset)   & 0xff) as u8,
-                    alpha : ((pixel_value  >> alpha_offset) & 0xff) as u8,
-                };
-
-                if alpha_mask == 0x00 {
-                    // NOTE(erick): We are in XRGB mode.
-                    pixel.alpha = 0xff;
-                }
-
-                result.push(pixel);
-            }
+            bitmap_read::read_32_bitfield(&mut data_walker, &mut result,
+                                         red_mask, green_mask,
+                                         blue_mask, alpha_mask);
         } else if bits_per_pixel == 16 {
-            let mut column_index = 0;
-            while data_walker.has_data() {
-                if column_index == info_header.image_width {
-                    // NOTE(erick): We have to align rows to
-                    // 4 bytes values.
-                    column_index = 0;
-                    data_walker.align_with_u32();
-                }
-
-                // NOTE(erick): The file can have some padding at the end.
-                if !data_walker.has_data() {
-                    break;
-                }
-
-                let pixel_value = data_walker.next_u16() as u32;
-
-                let mut pixel = BitmapPixel {
-                    blue  : ((pixel_value & blue_mask)  >> blue_offset)  as u8,
-                    green : ((pixel_value & green_mask) >> green_offset) as u8,
-                    red   : ((pixel_value & red_mask)   >> red_offset)   as u8,
-                    alpha : ((pixel_value & alpha_mask) >> alpha_offset) as u8,
-                };
-
-                map_zero_based(&mut pixel.red   , red_shifted, 0xff);
-                map_zero_based(&mut pixel.green , green_shifted, 0xff);
-                map_zero_based(&mut pixel.blue  , blue_shifted, 0xff);
-                map_zero_based(&mut pixel.alpha , alpha_shifted, 0xff);
-                // HACK: Since your map function is incorrect, we hardcode
-                // the alpha mapping.
-                // if pixel.alpha == 1 {
-                //     pixel.alpha = 0xff;
-                // }
-
-                if alpha_mask == 0x00 {
-                    // NOTE(erick): We are in XRGB mode.
-                    pixel.alpha = 0xff;
-                }
-
-                result.push(pixel);
-                column_index += 1;
-            }
+            bitmap_read::read_16_bitfield(&mut data_walker, &mut result,
+                                         info_header.image_width,
+                                         red_mask, green_mask,
+                                         blue_mask, alpha_mask);
         } else {
             panic!("BitField is only compatible with 16 and 32 bit. Got: {}",
                    bits_per_pixel);
         }
     } else if compression_type == CompressionType::Uncompressed as u32 {
         if bits_per_pixel == 32 {
-            // NOTE(erick): We only have alpha when the
-            // compression_type is BitFields. The last byte is
-            // here only for padding.
-            while data_walker.has_data() {
-                let pixel = BitmapPixel {
-                    blue  : data_walker.next_u8(),
-                    green : data_walker.next_u8(),
-                    red   : data_walker.next_u8(),
-                    alpha : 0xff,
-                };
-                // NOTE(erick): We have to discard the padding byte.
-                data_walker.next_u8();
-                result.push(pixel);
-            }
+            bitmap_read::read_32_uncompressed(&mut data_walker, &mut result);
         } else if bits_per_pixel == 24 {
-            let mut column_index = 0;
-            while data_walker.has_data() {
-                if column_index == info_header.image_width {
-                    // NOTE(erick): We have to align rows to
-                    // 4 bytes values.
-                    column_index = 0;
-                    data_walker.align_with_u32();
-
-                    // NOTE(erick): The file can have some padding at the end.
-                    if !data_walker.has_data() {
-                        break;
-                    }
-                }
-
-                let pixel = BitmapPixel {
-                    blue  : data_walker.next_u8(),
-                    green : data_walker.next_u8(),
-                    red   : data_walker.next_u8(),
-                    alpha : 0xff,
-                };
-
-                result.push(pixel);
-                column_index += 1;
-            }
+            bitmap_read::read_24_uncompressed(&mut data_walker, &mut result,
+                                             info_header.image_width);
         } else if bits_per_pixel == 8 {
-            let image_palette = palette.as_ref().unwrap();
-
-            let mut column_index = 0;
-            while data_walker.has_data() {
-                if column_index == info_header.image_width {
-                    column_index = 0;
-                    data_walker.align_with_u32();
-
-                    if !data_walker.has_data() {
-                        break;
-                    }
-                }
-                let pixel_index = data_walker.next_u8() as usize;
-                let pixel = image_palette[pixel_index];
-
-                result.push(pixel);
-                column_index += 1;
-            }
+            bitmap_read::read_8_uncompressed(&mut data_walker, &mut result,
+                                            info_header.image_width,
+                                            palette.as_ref().unwrap());
         }
         else if bits_per_pixel == 4 {
-            let image_palette = palette.as_ref().unwrap();
+            bitmap_read::read_4_uncompressed(&mut data_walker, &mut result,
+                                            info_header.image_width,
+                                            palette.as_ref().unwrap());
 
-            let mut column_index = 0;
-            while data_walker.has_data() {
-                if column_index >= info_header.image_width {
-                    column_index = 0;
-                    data_walker.align_with_u32();
-
-                    if !data_walker.has_data() {
-                        break;
-                    }
-                }
-                let pixels_indexes = data_walker.next_u8();
-                let p0_index = (pixels_indexes >> 4) as usize;
-                let p1_index = (pixels_indexes & 0x0f) as usize;
-
-                let pixel0 = image_palette[p0_index];
-                let pixel1 = image_palette[p1_index];
-
-                result.push(pixel0);
-                column_index += 1;
-
-                if column_index < info_header.image_width {
-                    result.push(pixel1);
-                    column_index += 1;
-                }
-            }
         } else if bits_per_pixel == 1 {
-            let image_palette = palette.as_ref().unwrap();
-
-            for _ in 0 .. info_header.image_height {
-                let mut column_index = 0;
-                for _ in 0 .. info_header.image_width / 8 {
-                    let pixels_byte = data_walker.next_u8();
-                    let mut pixels = get_pixels_from_byte(&image_palette,
-                                                      pixels_byte, 8);
-
-                    result.append(&mut pixels);
-                    column_index += 8;
-                }
-
-                let remaining_pixels = info_header.image_width - column_index;
-                if remaining_pixels > 0 {
-                    let pixels_byte = data_walker.next_u8();
-                    let mut pixels = get_pixels_from_byte(&image_palette,
-                                                          pixels_byte,
-                                                          remaining_pixels);
-
-                    result.append(&mut pixels);
-                }
-
-                data_walker.align_with_u32()
-            }
+            bitmap_read::read_1_uncompressed(&mut data_walker, &mut result,
+                                            info_header.image_width,
+                                            info_header.image_height,
+                                            palette.as_ref().unwrap());
 
         } else {
             panic!("Error: {} bits is not a valid format.", bits_per_pixel);
@@ -612,30 +457,6 @@ fn interpret_image_data(data: &[u8],
     }
 
     result
-}
-
-fn get_pixels_from_byte(palette: &BitmapPalette,
-                        byte: u8, n_bits: i32) -> Vec<BitmapPixel> {
-    let mut result = Vec::new();
-    let mut mask = 0x80;
-
-    for _ in 0 .. n_bits {
-        if byte & mask == 0 {
-            result.push(palette[0]);
-        } else {
-            result.push(palette[1]);
-        }
-
-        mask = mask >> 1;
-    }
-
-    result
-}
-
-macro_rules! pad_to_align {
-    ($value:expr, $alignment:expr) => (
-        ($alignment - ($value % $alignment)) % $alignment
-    )
 }
 
 fn pixels_into_data(pixels: &Vec<BitmapPixel>, data: &mut Vec<u8>,
@@ -1243,68 +1064,6 @@ fn mirror_slice<T>(slice: &mut [T]) where T: Copy {
         let tmp = slice[index_left];
         slice[index_left] = slice[index_right];
         slice[index_right] = tmp;
-    }
-}
-
-struct BytesWalker<'a> {
-    data          : &'a [u8],
-    current_index : usize,
-}
-
-impl<'a> BytesWalker<'a> {
-    fn new(d: &[u8]) -> BytesWalker {
-        BytesWalker {
-            data          : d,
-            current_index : 0,
-        }
-    }
-
-    fn has_data(&self) -> bool {
-        self.current_index < self.data.len()
-    }
-
-    fn next_u8(&mut self) -> u8 {
-        let result = self.data[self.current_index];
-        self.current_index += 1;
-
-        result
-    }
-
-    // NOTE(erick): It would be nice to use generics to
-    // generate this functions, but I don't know of
-    // a way to get the size of a type at compile time.
-    // WARNING(erick): Theses functions only work
-    // because the bitmap format uses little-endianness
-    // and we are running on an little-endian machine.
-    // Sooner or later this will have to be fixed.
-    fn next_u16(&mut self) -> u16 {
-        let mut bytes = [0; 2];
-        bytes.clone_from_slice(&self.data[self.current_index .. self.current_index + 2]);
-        self.current_index += 2;
-
-        unsafe { transmute(bytes) }
-    }
-
-    fn next_u32(&mut self) -> u32 {
-        let mut bytes = [0; 4];
-        bytes.clone_from_slice(&self.data[self.current_index .. self.current_index + 4]);
-        self.current_index += 4;
-
-        unsafe { transmute(bytes) }
-    }
-
-    fn next_i32(&mut self) -> i32 {
-        let mut bytes = [0; 4];
-        bytes.clone_from_slice(&self.data[self.current_index .. self.current_index + 4]);
-        self.current_index += 4;
-
-        unsafe { transmute(bytes) }
-    }
-
-    fn align_with_u32(&mut self) {
-        let pad = pad_to_align!(self.current_index, 4);
-
-        self.current_index += pad;
     }
 }
 
